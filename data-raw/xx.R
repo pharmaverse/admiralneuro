5b972468cc1a52002fafdd9707bcc4c8c243b3b3
Edoardo Mancini <edoardo.mancini@roche.com>
2025-02-27 10:40:55 +0000

    #1 get checks to pass

--- data-raw/xx.R
+++ data-raw/xx.R
@@ -4,4 +4,4 @@
 # Add code to create test dataset xx and update this in `R/data.R`
 
 # Save dataset ----
-# usethis::use_data(xx, overwrite = TRUE)
+# usethis::use_data(xx, overwrite = TRUE) #nolint
