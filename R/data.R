5e3e255de08b71b4a3ea103987cf0b50c097a1c0
Jian Wang <wang_jian_wj@lilly.com>
2025-03-23 11:06:59 -0400

    Added nv_neuro dataset information #7

--- R/data.R
+++ R/data.R
@@ -4,3 +4,15 @@
 #' @keywords datasets
 #' @family datasets
 "dm_neuro"
+
+
+#' Example NV SDTM dataset
+#'
+#' An example NV SDTM dataset for Alzheimer's disease observational and interventional studies. The amyloid and tau PET data at two timepoints reflect levels of pathology appropriate for disease or treatment course
+#'
+#' @keywords datasets
+#'
+#' @family datasets
+#'
+#' @source Constructed using `dm_neuro` from `{admiralneuro}` package for USUBJID, `vs` from `{pharmaversesdtm}` for VISITNUM
+"nv_neuro"
