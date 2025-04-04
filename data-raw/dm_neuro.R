5df11ac62f54f01d2150a0a0decc62b894c30af4
Chris Pelentrides <chris.pelentrides@roche.com>
2025-04-04 13:56:30 +0000

    feat: #13 Improvement of dm_neuro to include observational study data and imputed RFICDTC

--- data-raw/dm_neuro.R
+++ data-raw/dm_neuro.R
@@ -4,6 +4,8 @@
 # Load libraries -----
 library(dplyr)
 library(admiral)
+library(haven)
+library(lubridate)
 
 # Read input test data from pharmaversesdtm ----
 dm <- pharmaversesdtm::dm
@@ -11,18 +13,53 @@ dm <- pharmaversesdtm::dm
 # Convert blank to NA ----
 dm <- convert_blanks_to_na(dm)
 
-# Subset to first 15 patients who fall between ages 58 and 79, Placebo and
-# Xanomeline High Dose only
+# Subset to 15 patients
 dm_neuro <- dm %>%
   dplyr::filter(USUBJID %in% c(
     "01-701-1015", "01-701-1023", "01-701-1028", "01-701-1034",
     "01-701-1146", "01-701-1153", "01-701-1181", "01-701-1234",
     "01-701-1275", "01-701-1302", "01-701-1345", "01-701-1360",
-    "01-701-1383", "01-701-1392", "01-701-1440"
+    "01-701-1383", "01-701-1392", "01-714-1288"
   ))
+
+# Create a vector of USUBJID values to modify
+usubjid_to_modify <- c("01-701-1345", "01-714-1288" , "01-701-1028",
+                       "01-701-1181", "01-701-1360")
+
+# Store the labels before modifying
+var_labels <- lapply(dm_neuro, function(x) attr(x, "label"))
+
+# Modify the dataset
+dm_neuro <- dm_neuro %>%
+  mutate(
+    across(c(ARMCD, ARM, ACTARMCD, ACTARM, RFXSTDTC, RFXENDTC),
+           ~ ifelse(USUBJID %in% usubjid_to_modify, NA_character_, .),
+           .names = "{.col}"),
+
+    ARMNRS = case_when(
+      USUBJID %in% usubjid_to_modify ~ "Observational Study",
+      TRUE ~ NA_character_
+    ),
+
+    # Convert RFSDTC from char to date, -2 days, and convert back to char
+    RFICDTC = if_else(
+      !is.na(RFSTDTC),  # Check if RFSDTC is not NA
+      as.character(as.Date(RFSTDTC, format = "%Y-%m-%d") - days(2)),
+      NA_character_  # Return NA if RFSDTC is NA
+    )
+  )
+
+# Restore the labels after modification
+for (col in names(var_labels)) {
+  attr(dm_neuro[[col]], "label") <- var_labels[[col]]
+}
+
+# Assign the label for ARMNRS
+attr(dm_neuro$ARMNRS, "label") <- "Reason Arm and/or Actual Arm is Null"
 
 # Label dataset ----
 attr(dm_neuro, "label") <- "Demographics"
+attr(dm_neuro, "variable.labels") <- var_labels
 
 # Save dataset ----
 usethis::use_data(dm_neuro, overwrite = TRUE)
